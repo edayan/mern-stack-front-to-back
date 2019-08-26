b00238b0c0e804fce4628676908d25b7a42ee8e8
Saju Paul <saju.christel@blu-pa.com>
2019-08-27 00:23:59 +0530

    Connect to Mongo DB

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log(`Mongo DB connected`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
