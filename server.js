cf99997837bd3d02c398a9d201be3b57e8021a34
Saju Paul <saju.christel@blu-pa.com>
2019-08-27 18:54:22 +0530

    Adding routes

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ connectDb();
 
 app.get(`/`, (req, res) => res.send(`Api Running`));
 
-const PORT = process.env.PORT || 5000;
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started at port:${PORT}`));
